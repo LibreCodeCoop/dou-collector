326430fb8acebaac5c9bd69c69958c948b42a7e1
Vitor Mattos <vitor@php.rio>
2020-01-15 18:27:08 -0300

    Adição de licença Documentação do projeto no composer.json para publicação de pacote Implementação de phpstan

--- src/DOU.php
+++ src/DOU.php
@@ -2,19 +2,39 @@
 namespace DouCollector;
 
 use Symfony\Component\BrowserKit\HttpBrowser;
+use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\HttpClient\HttpClient;
 
 class DOU
 {
+    /**
+     * @var HttpBrowser
+     */
     public $client;
+    /**
+     * @var string
+     */
     private $baseUrl;
-    private $maxRequests;
+    /**
+     * @var integer
+     */
+    private $maxRequests = 0;
+    /**
+     * @param array{baseUrl:string, maxRequests:int} $settings
+     */
     public function __construct(array $settings)
     {
         $this->baseUrl = $settings['baseUrl'];
         $this->maxRequests = $settings['maxRequests'];
         $this->client = new HttpBrowser(HttpClient::create());
     }
+    /**
+     * Search by date using a list of search strings
+     *
+     * @param string $date Format d-m-Y
+     * @param array{string} $monitoringKeys Array with search strings
+     * @return array{list:array} List containing sets of results
+     */
     public function collectData(string $date, array $monitoringKeys):array
     {
         $this->client->setServerParameter('HTTP_USER_AGENT', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36');
@@ -31,7 +51,7 @@ class DOU
         $licitacoes = [];
         $requestCount = 0;
         if (!empty($obj->jsonArray)) {
-            $licitacoes = array_filter($obj->jsonArray, function($v) use ($monitoringKeys, &$requestCount) {
+            $licitacoes = array_filter($obj->jsonArray, function ($v) use ($monitoringKeys, &$requestCount) {
                 if ($this->maxRequests && $requestCount >= $this->maxRequests) {
                     return false;
                 }
@@ -46,20 +66,24 @@ class DOU
                 }
                 return $exists;
             });
-        }
-        foreach ($licitacoes as $key => $data) {
-            $licitacoes[$key] = $this->populateDetails($data);
+            array_walk($licitacoes, [$this, 'populateDetails']);
         }
         return ['list' => $licitacoes];
     }
 
-    private function populateDetails(\stdClass $data)
+    private function populateDetails(\stdClass $data): \stdClass
     {
         $crawler = $this->client->request('GET', $this->baseUrl . '/web/dou/-/' . $data->urlTitle);// 11-12-2019
         $a = $crawler->filter('.botao-materia a');
         if ($a->count()) {
-            parse_str(parse_url($a->attr('href'))['query'], $query);
-            $data->jornal = $query['jornal'];
+            $href = $a->attr('href');
+            if ($href) {
+                $urlParsed = parse_url($href);
+                if ($urlParsed) {
+                    parse_str($urlParsed['query'], $query);
+                    $data->jornal = $query['jornal'];
+                }
+            }
         }
 
         $this->populateElement($crawler, $data, '.publicado-dou-data');
@@ -74,16 +98,21 @@ class DOU
         return $data;
     }
 
-    private function populateElement($crawler, &$data, $selector)
+    private function populateElement(Crawler $crawler, \stdClass &$data, string $selector): void
     {
         $elements = $crawler->filter($selector);
         if ($elements->count()) {
             $property = str_replace('.', '', $selector);
             $property = str_replace('-', '_', $property);
-            $children = $elements->getNode(0)->childNodes;
-            $data->{$property} = '';
-            foreach ($children as $child) {
-                $data->{$property}.= $elements->getNode(0)->ownerDocument->saveHTML($child);
+            $node = $elements->getNode(0);
+            if ($node) {
+                $children = $node->childNodes;
+                $data->{$property} = '';
+                foreach ($children as $child) {
+                    if ($node->ownerDocument) {
+                        $data->{$property}.= $node->ownerDocument->saveHTML($child);
+                    }
+                }
             }
         }
     }
