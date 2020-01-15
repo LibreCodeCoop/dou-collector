41ebbe2387a1c1ea243274fae598f3675a338a09
Vitor Mattos <vitor@php.rio>
2020-01-14 22:41:22 -0300

    Versão estável, refatorada e com testes

--- src/Locale/DOU.php
+++ src/Locale/DOU.php
@@ -7,17 +7,19 @@ use Symfony\Component\HttpClient\HttpClient;
 class DOU
 {
     public $client;
-    public function __construct()
+    private $baseUrl;
+    private $maxRequests;
+    public function __construct(array $settings)
     {
+        $this->baseUrl = $settings['baseUrl'];
+        $this->maxRequests = $settings['maxRequests'];
         $this->client = new HttpBrowser(HttpClient::create());
     }
-    public function collectData(string $date):array
+    public function collectData(string $date, array $monitoringKeys):array
     {
         $this->client->setServerParameter('HTTP_USER_AGENT', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36');
 
-        $crawler = $this->client->request('GET', 'http://www.in.gov.br/leiturajornal?secao=do3&data=' . $_GET['data']);// 11-12-2019
-        
-        $fullPageHtml = $crawler->html();
+        $crawler = $this->client->request('GET', $this->baseUrl . '/leiturajornal?secao=do3&data=' . $date);// 11-12-2019
         
         $json = $crawler->filter('#params');
         if (!$json->count()) {
@@ -27,15 +29,55 @@ class DOU
         $obj = json_decode($json);
         
         $licitacoes = [];
+        $requestCount = 0;
         if (!empty($obj->jsonArray)) {
-            $licitacoes = array_filter($obj->jsonArray, function($v) {
-                $return = false;
-                if (isset($v->artType)) {
-                    $return = strpos(strtolower($v->artType), 'aviso de licita') !== false;
+            $licitacoes = array_filter($obj->jsonArray, function($v) use ($monitoringKeys, &$requestCount) {
+                if ($this->maxRequests && $requestCount >= $this->maxRequests) {
+                    return false;
                 }
-                return $return;
+                $exists = false;
+                if (isset($v->artType)) {
+                    foreach ($monitoringKeys as $key) {
+                        $exists = strpos(strtolower($v->artType), $key) !== false;
+                    }
+                }
+                if ($this->maxRequests && $exists) {
+                    $requestCount++;
+                }
+                return $exists;
             });
         }
+        foreach ($licitacoes as $key => $data) {
+            $licitacoes[$key] = $this->populateDetails($data);
+        }
         return ['list' => $licitacoes];
+    }
+
+    private function populateDetails(\stdClass $data)
+    {
+        $crawler = $this->client->request('GET', $this->baseUrl . '/web/dou/-/' . $data->urlTitle);// 11-12-2019
+
+        $this->populateElement($crawler, $data, '.edicao-dou-data');
+        $this->populateElement($crawler, $data, '.identifica');
+        $this->populateElement($crawler, $data, '.dou-paragraph');
+        $this->populateElement($crawler, $data, '.assina');
+        $this->populateElement($crawler, $data, '.cargo');
+        $this->populateElement($crawler, $data, '.informacao-conteudo-dou');
+        $this->populateElement($crawler, $data, '.texto-dou');
+
+        return $data;
+    }
+
+    private function populateElement($crawler, &$data, $selector)
+    {
+        $elements = $crawler->filter($selector);
+        if ($elements->count()) {
+            $property = str_replace('.', '', $selector);
+            $property = str_replace('-', '_', $property);
+            $children = $elements->getNode(0)->childNodes;
+            foreach ($children as $child) {
+                $data->{$property}.= $elements->getNode(0)->ownerDocument->saveHTML($child);
+            }
+        }
     }
 }
