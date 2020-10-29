a73ebdfc0e7afd64b5106d8bc83701318ec3bdc4
Fernando M. Tenório <dotenorio@gmail.com>
2020-10-29 00:21:26 -0300

    Adicionando a opçao de rodar por linha de comando

---
+++ src/Commands/ExportCommand.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace DouCollector\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use DouCollector\DOU;
+use Spatie\ArrayToXml\ArrayToXml;
+
+class ExportCommand extends Command
+{
+    const FORMAT_WHITELIST = ["json", "xml"];
+
+    protected function configure()
+    {
+        $this
+            ->setName('export')
+            ->setDescription('Exporta dados do DOU')
+            ->addOption(
+                'date',
+                'd',
+                InputOption::VALUE_REQUIRED,
+                'Data para realizar importação, exemplo: 30-12-2019'
+            )
+            ->addOption(
+                'keywords',
+                'k',
+                InputOption::VALUE_REQUIRED,
+                'Lista de palavras-chave para importar, separadas por vírgula.'
+            )
+            ->addOption(
+                'format',
+                'f',
+                InputOption::VALUE_OPTIONAL,
+                'O formato de saída (json, xml). [default: "json"]'
+            )
+            ->addOption(
+                'maxRequests',
+                'm',
+                InputOption::VALUE_OPTIONAL,
+                'Máximo de requisições que serão feitas, se 0 fará quantas for necessário. [default: 3]'
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $date = $input->getOption('date');
+        $keywords = explode(",", $input->getOption('keywords'));
+        $format = $input->getOption('format') or $format = 'json';
+        $maxRequests = $input->getOption('maxRequests') or $maxRequests = 3;
+
+        if (!$date) {
+            $output->writeln('A opção "date" é obrigatória.');
+            return Command::FAILURE;            
+        }
+        
+        if (empty($keywords) || $keywords[0] == "") {
+            $output->writeln('A opção "keywords" é obrigatória.');
+            return Command::FAILURE;            
+        }
+
+        if (!in_array($format, self::FORMAT_WHITELIST)) {
+            $output->writeln([
+                "Formato \"{$format}\" é inválido!",
+                'Os formatos válidos são: ' . implode(", ", self::FORMAT_WHITELIST)
+            ]);
+            return Command::FAILURE;
+        }
+
+        $DOU = new DOU([
+            'maxRequests' => $maxRequests
+        ]);
+        foreach ($DOU->collectData($date, $keywords) as $result) {
+            $results[] = $result;
+        }
+        
+        $json = json_encode($results);
+
+        if ($format == 'json') {
+            $output->writeln($json);
+            return Command::SUCCESS;
+        }
+        
+        if ($format == 'xml') {
+            $array = json_decode($json, true);
+            $arrayWithValidKey = [];
+            foreach ($array as $item) {
+                $arrayWithValidKey[$item['urlTitle']] = $item;
+            }
+            $xml = ArrayToXml::convert($arrayWithValidKey, 'results');
+            $output->writeln($xml);
+            return Command::SUCCESS;
+        }
+    }
+}
